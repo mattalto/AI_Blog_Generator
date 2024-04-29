8c9912514527c748dd1973cbabfba561ce88a191
mattalto <mcaaltomare@gmail.com>
2024-04-28 22:04:23 -0400

    Backend folder set up with Django and homepage URL defined

---
+++ backend/ai_blog_generator/blog_generator/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+# Store all the URLs for the app here
+urlpatterns = [
+    path('', views.index, name='index') # When user goes to the home page, we'll go into the views file and look for function called 'index' and whatever that function does will be rendered
+]
